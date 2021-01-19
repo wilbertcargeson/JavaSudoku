f9924f4224390523dfea81f19855f5ba6321f8ef
Wilbert Cargeson <wcargeson@student.unimelb.edu.au>
2021-01-19 21:22:23 +1100

    Completed UserInterface package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Sudoku desktop game using java
+
+This code is part of my practice while learning from freeCodeCamp.org
--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -1,4 +1,36 @@
 package sudoku.problemdomain;
 
+import java.util.Objects;
+
 public class Coordinates {
+
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+
+        Coordinates that = (Coordinates) o;
+        return ( that.x == this.x && that.y == this.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x,y);
+    }
 }
