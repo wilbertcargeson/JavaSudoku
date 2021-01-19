291b764d09f65ff450f70d2fc1fc6ed2034df422
Wilbert Cargeson <wcargeson@student.unimelb.edu.au>
2021-01-19 22:42:07 +1100

    Created UI

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,7 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congratz you won";
+    public static final String ERROR = "An error has occurred";
+
+}
